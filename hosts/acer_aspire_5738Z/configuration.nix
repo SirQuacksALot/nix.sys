a08b3e9a308f681e5d53f7d6d2b0d3bb8b49888e
Sebastian <sebastian.glass3@gmail.com>
2025-07-22 09:54:37 +0200

    enabled experimental features for acer aspire 5738Z

--- hosts/acer_aspire_5738Z/configuration.nix
+++ hosts/acer_aspire_5738Z/configuration.nix
@@ -10,6 +10,9 @@
       ./hardware-configuration.nix
     ];
 
+  # Experimental Features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
