2fc7aa4c660dad6864097ee2739364ad947195de
Sebastian <sebastian.glass3@gmail.com>
2025-07-22 15:20:40 +0200

    made username dynamic

--- hosts/acer_aspire_5738Z/configuration.nix
+++ hosts/acer_aspire_5738Z/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 
 {
   imports =
@@ -70,9 +70,9 @@
   # console.keyMap = "de";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sebastian = {
+  users.users.${username} = {
     isNormalUser = true;
-    description = "sebastian";
+    description = "${username}";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
