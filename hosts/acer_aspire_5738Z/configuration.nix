0419e7b547d08123e728b910c0becd74fa8737fc
Sebastian <sebastian.glass3@gmail.com>
2025-07-22 14:03:03 +0200

    fixed misspelling

--- hosts/acer_aspire_5738Z/configuration.nix
+++ hosts/acer_aspire_5738Z/configuration.nix
@@ -9,8 +9,8 @@
     [ # Include the results of the hardware scan.
       ../base-features.nix
       ./hardware-configuration.nix
-      ../../environments/desktop/base
-      ../../environments/dekstop/hyprland
+      ../../environments/desktops/base
+      ../../environments/dekstops/hyprland
     ];
 
   # Experimental Features
