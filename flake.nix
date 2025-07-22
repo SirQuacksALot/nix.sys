ff994ac3515bca81fcf5d257f2494a7212f51026
Sebastian <sebastian.glass3@gmail.com>
2025-07-22 10:45:50 +0200

    fixed misspellings

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Main System Flake"
+  description = "Main System Flake";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
@@ -8,7 +8,7 @@
     pkgs = import nixpkgs { inherit system; };
   in {
     # --------------[ .Systems ]-------------
-    nixosConfiurations.acerAspire5738Z = nixpks.lib.nixosSystem {
+    nixosConfiurations.acerAspire5738Z = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit system; };
       modules = [
         ./hosts/acer_aspire_5738Z/configuration.nix
@@ -16,11 +16,11 @@
     };        
 
     # ---------------[ Shells ]--------------
-    devShells.x86_6-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [ hello ];
-      shellHook = ''
-        echo "Welcome to the devShell!"
-      '';
-    };
+    # devShells.x86_6-linux.default = pkgs.mkShell {
+    #   buildInputs = with pkgs; [ hello ];
+    #   shellHook = ''
+    #     echo "Welcome to the devShell!"
+    #   '';
+    # };
   };
 }
