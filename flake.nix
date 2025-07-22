70f03673a0913bc3f0f9d06a7432a773d6744215
Sebastian <sebastian.glass3@gmail.com>
2025-07-22 13:51:41 +0200

    updated system flake structure

--- flake.nix
+++ flake.nix
@@ -1,23 +1,42 @@
 {
   description = "Main System Flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  # ------------[ System Inputs ]--------------
+  inputs.nixpkgs.url 	      = "github:nixos/nixpkgs?ef=nixos-25.05";       # stable version
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";   # roling
+  inputs.nixos-hardware.url   = "github:nixos/nixos-hardware/master";        # hardware tweaks (optinal)
+  inputs.home-manager.url     = "github:nix-community/home-manager";         # home manager
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, ... }: let
+  # ------------[ System Outputs ]-------------
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ... }: let
+    # --------[ Basic definitions ]------------
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    host = "acer_aspire_5738Z";
+
+    # -----[ Package Manager definition ]------
+    pkgs = import nixpkgs { 
+      inherit system;
+      config.allowUnfree = true;
+      config.android_sdk.accept_license = true;
+      overlays = [ (final: _: {
+	unstable = import nixpkgs-unstable {
+	  inherit (final.stenv.hostPlatform) system;
+	  inherit (final) config;
+          };
+	}) ];
+    };
   in {
-    # --------------[ .Systems ]-------------
-    nixosConfigurations.acerAspire5738Z = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit system; };
-      modules = [
-        ./hosts/acer_aspire_5738Z/configuration.nix
-        ./environments/desktops/base
-        ./environments/desktops/hyprland
-      ];
+    # ---------------[ System ]---------------- 
+    nixosConfigurations.${host} = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit system home-manager; };
+      modules = [ ./hosts/${host}/configuration.nix ];
     };        
 
-    # ---------------[ Shells ]--------------
+    # -------------[ Dev Shells ]--------------
+    #
+    # spaeter in eigenen flake auslagern
+    #
     # devShells.x86_6-linux.default = pkgs.mkShell {
     #   buildInputs = with pkgs; [ hello ];
     #   shellHook = ''
